009bfabb9c5c6d8fd81b40dded0201b31c7dea7b
Shreyas Sahoo <shreyassahoo26@gmail.com>
2024-05-09 14:20:46 +0530

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Shreyas Sahoo on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
